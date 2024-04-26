31ebdc752efbd6d774d1d73ed69511deaac08764
Ale579 <alessandrotg9@gmail.com>
2024-04-26 11:09:33 +0200

    Add comments to structure program

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+// Play 5 rounds
+
+    // Play round
+
+        // Get computer's choice
+
+        // Get the human's choice
+
+        // Compare human's choice with computer's choice, declare outcome
+        // and modify scores
