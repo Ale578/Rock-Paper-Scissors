65ec757df00cc7f2561fef281bfdf8217852c23f
Ale579 <alessandrotg9@gmail.com>
2024-04-26 11:41:51 +0200

    Add getComputerChoice and getHumanChoice function

--- game.js
+++ game.js
@@ -3,8 +3,38 @@
     // Play round
 
         // Get computer's choice
+        let computer_selection = getComputerChoice();
+        console.log(computer_selection);
 
         // Get the human's choice
+        let human_selection = getHumanChoice();
+        console.log(human_selection);
 
         // Compare human's choice with computer's choice, declare outcome
         // and modify scores
+
+
+function getComputerChoice() {
+    // Return randomly choose rock, paper or scissors 
+    let random_value = Math.floor(Math.random() * 2);
+
+    if (random_value == 0) {
+        return "rock"
+    }
+    else if (random_value == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function getHumanChoice () {
+    // Prompt user until user inputs rock, paper or scissors
+    let user_input;
+    do {
+        user_input = prompt("Input rock, paper or scissors:").toLowerCase();
+    } while ((user_input != "rock") && (user_input != "paper") && (user_input != "scissors"));  
+    
+    return user_input
+}
