7efbc776fa2618dd797ac969b9ea715f61a7a97f
Ale579 <alessandrotg9@gmail.com>
2024-04-26 12:18:14 +0200

    Create playGame function and move all other function declarations to playGame

--- game.js
+++ game.js
@@ -1,22 +1,36 @@
-// Play 5 rounds
 let human_score, computer_score = 0;
 
-    // Play round
+function playGame () {
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        // Play round
 
-        // Get the human's choice
+            // Get the human's choice
         let human_selection = getHumanChoice();
         console.log(human_selection);
 
-        // Get computer's choice
+            // Get computer's choice
         let computer_selection = getComputerChoice();
         console.log(computer_selection);
-        
+    
 
-        // Compare human's choice with computer's choice, declare outcome
-        // and modify scores
+            // Compare human's choice with computer's choice, declare outcome
+            // and modify scores
         playRound(human_selection, computer_selection);
+    }
+    // Declare match winner
+    if (human_score == computer_score) {
+        console.log("The game was a tie")
+    }
+    else if (human_score > computer_score) {
+        console.log("You won the match!")
+    }
+    else {
+        console.log("You lost the match...")
+    }
+}
 
-        
+
 function getHumanChoice () {
     // Prompt user until user inputs rock, paper or scissors
     let user_input;
@@ -61,3 +75,5 @@ function playRound(humanChoice, computerChoice) {
         computer_score += 1;
     }
 }
+
+playGame();
