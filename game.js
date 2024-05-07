054c7ae1679da64752b56eaaaaee55d3e172d7a6
Ale579 <alessandrotg9@gmail.com>
2024-05-07 11:04:33 +0200

    Add button UI

--- game.js
+++ game.js
@@ -1,23 +1,40 @@
 let human_score, computer_score = 0;
 
 function playGame () {
-    // Play 5 rounds
-    for (let i = 0; i < 5; i++) {
+    // Play to 5 points
+
         // Play round
 
             // Get the human's choice
-        let human_selection = getHumanChoice();
+
+    let human_selection;
+    let options = document.querySelector('#options');
+
+    options.addEventListener('click', (event) => {
+        let target = event.target;
+
+        switch(target.id) {
+            case "rock":
+                human_selection = "rock";
+                break;
+            case "paper":
+                human_selection = "paper";
+                break;
+            case "scissors":
+                human_selection = "scissors";
+                break;
+        }
         console.log(human_selection);
 
             // Get computer's choice
         let computer_selection = getComputerChoice();
         console.log(computer_selection);
-    
 
             // Compare human's choice with computer's choice, declare outcome
             // and modify scores
         playRound(human_selection, computer_selection);
-    }
+
+    });
     // Declare match winner
     if (human_score == computer_score) {
         console.log("The game was a tie")
@@ -29,18 +46,6 @@ function playGame () {
         console.log("You lost the match...")
     }
 }
-
-
-function getHumanChoice () {
-    // Prompt user until user inputs rock, paper or scissors
-    let user_input;
-    do {
-        user_input = prompt("Input rock, paper or scissors:").toLowerCase();
-    } while ((user_input != "rock") && (user_input != "paper") && (user_input != "scissors"));  
-    
-    return user_input
-}
-        
 
 function getComputerChoice() {
     // Return randomly choose rock, paper or scissors 
